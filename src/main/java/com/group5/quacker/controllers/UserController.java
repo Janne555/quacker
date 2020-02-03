3f846ca0c5fce928e2ba76e213bf8859e1fef637
schatze <oskar@schatz.fi>
2020-02-03 11:13:11 +0200

    new file:   src/main/java/com/group5/quacker/controllers/UserController.java 	new file:   src/main/resources/templates/userpage.html

---
+++ src/main/java/com/group5/quacker/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.group5.quacker.controllers;
+
+import com.group5.quacker.entities.User;
+import com.group5.quacker.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public String userPageGet(@PathVariable("name") String name, Model model) {
+
+        User user = userRepository.findByName(name);
+        if(user==null)
+            return "redirect:/";
+
+        model.addAttribute("user", user);
+
+        if (user.getProfilePhoto() != null) {
+            model.addAttribute("profilePhotoUrl", "/files/" + user.getProfilePhoto().getPublicId());
+        }
+
+        return "userpage";
+    }
+
+}
