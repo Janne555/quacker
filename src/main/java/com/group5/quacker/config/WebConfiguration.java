8823e3f7749b893ca3ca1b69997491778c104172
Janne Lavila <janne555@users.noreply.github.com>
2020-02-06 11:25:35 +0200

    add a bunch of stuff

--- src/main/java/com/group5/quacker/config/WebConfiguration.java
+++ src/main/java/com/group5/quacker/config/WebConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -28,4 +29,12 @@ public class WebConfiguration implements WebMvcConfigurer {
 
         return bean;
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(
+                "/css/**")
+                .addResourceLocations("classpath:/static/css/");
+    }
+
 }
