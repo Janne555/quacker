b966a1742232e2eb8a1c0f8e8f3ad968cccc7480
Janne Lavila <janne555@users.noreply.github.com>
2020-02-06 20:18:06 +0200

    add more settings

--- src/main/java/com/group5/quacker/controllers/SettingsController.java
+++ src/main/java/com/group5/quacker/controllers/SettingsController.java
@@ -9,10 +9,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,8 +23,8 @@ public class SettingsController {
     @Autowired
     private FileService fileService;
 
-    @GetMapping("/settings/{setting}")
-    public String getSettings(@PathVariable("setting") String setting, Model model) {
+    @RequestMapping(value = {"/settings/{setting}", "/settings"}, method = RequestMethod.GET)
+    public String getSettings(@PathVariable(value = "setting", required = false) String setting, Model model) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         User user = userRepository.findByName(auth.getName());
@@ -41,19 +38,26 @@ public class SettingsController {
 
         model.addAttribute("user", user);
 
+        if (setting == null) {
+            return "settings";
+        }
+
         switch (setting) {
             case "profile-photo":
                 model.addAttribute("setting", "profile-photo");
                 break;
             case "personal-info":
                 model.addAttribute("setting", "personal-info");
+                break;
+            default:
+                break;
         }
 
         return "settings";
     }
 
     @PostMapping("/settings/profile-photo")
-    public String postProfilePhoto(@RequestParam("file") MultipartFile file ) throws IOException {
+    public String postProfilePhoto(@RequestParam("file") MultipartFile file) throws IOException {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 
         User user = userRepository.findByName(auth.getName());
