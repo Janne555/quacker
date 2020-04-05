d3b4119a83ec49cee312ae33caf2b99843201e71
Janne Lavila <janne555@users.noreply.github.com>
2020-04-05 12:49:07 +0300

    fix another test

--- src/main/java/com/group5/quacker/validators/CurrentPasswordValidator.java
+++ src/main/java/com/group5/quacker/validators/CurrentPasswordValidator.java
@@ -3,6 +3,7 @@ package com.group5.quacker.validators;
 import com.group5.quacker.constraints.CurrentPasswordConstraint;
 import com.group5.quacker.entities.User;
 import com.group5.quacker.repositories.UserRepository;
+import com.group5.quacker.services.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,7 +14,7 @@ import javax.validation.ConstraintValidatorContext;
 
 public class CurrentPasswordValidator implements ConstraintValidator<CurrentPasswordConstraint, String> {
     @Autowired
-    private UserRepository userRepository;
+    private AccountService accountService;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -24,12 +25,10 @@ public class CurrentPasswordValidator implements ConstraintValidator<CurrentPass
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext cxt) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        User user = userRepository.findByName(auth.getName());
+        User user = accountService.currentUser();
         if (user == null) {
             return false;
         } else {
-
             return passwordEncoder.matches(password, user.getPasswordHash());
         }
     }
